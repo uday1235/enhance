af03d0aca12d08d0758aa35fffb0d415108b1f85
Uday <udayshankar666@gmail.com>
2022-12-09 01:46:25 -0800

    Update Login.js

--- Login.js
+++ Login.js
@@ -38,7 +38,7 @@ function Login() {
       <Link to="/">
         <img
           className="login__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          src=""
         />
       </Link>
 
@@ -70,13 +70,13 @@ function Login() {
         </form>
 
         <p>
-          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          By signing-in you agree to the Hazlnut Conditions of Use &
           Sale. Please see our Privacy Notice, our Cookies Notice and our
           Interest-Based Ads Notice.
         </p>
 
         <button onClick={register} className="login__registerButton">
-          Create your Amazon Account
+          Create your hazlnut Account
         </button>
       </div>
     </div>
